dfd3131efa7b2eec0854839322ad191522bc89a6
Hung Tang <hungtang@hotmail.com>
2021-04-21 23:56:37 -0700

    baseline inquirer tested and added console.table package

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 const sql = require('mysql2');
-const db = require('./db/connection');
+// const db = require('./db/connection'); // this causes promise to break.
 
-require('dotenv').config(); //process.env.DB_User for example
+// require('dotenv').config(); //process.env.DB_User for example
 
-const entrySelect = ['view all departments', 'view all roles', 
-    'view all employees', 'add a department', 'add a role', 
-    'add an employee', 'update an employee role'];
+const entrySelect = ['View All Employees', 'View All Departments', 'View All Roles', 
+    'Add A Department', 'Add A Role', 'Add An Employee', 
+    'Update Employee Role', 'Update Employee Manager'];
+
+
+// Print to screen the name of app, and esc character for back-slash(\)
+console.log(`
+ ________________________________________________________
+|                                                        |
+|      _____                 _                           |
+|     |_____|  __ ___  ___  | | ___  _   _  ___  ___     |
+|     |  _| |/ _   _ \\|  _ \\| |/ _ \\| | | |/ _ \\/ _ \\    |
+|     | |___| | | | | | |_) | | ( ) | |_| |  __/  __/    |
+|     |_____|_| |_| |_| ___/|_|\\___/ \\__, |\\___|\\___|    |
+|                     |_|            |___/               |
+|      __  __                                            |
+|     |  \\/  | __ _ _ __   __ _  __ _  ___ _ __          |
+|     | |\\/| |/ _' | '_ \\ / _' |/ _' |/ _ \\ '__|         |
+|     | |  | | ( | | | | | ( | | (_| |  __/ |            |
+|     |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|            |
+|                               |___/                    |
+|                                                        |
+|________________________________________________________|
+
+`);
 
 const promptUser = () => {
     
@@ -14,7 +37,7 @@ const promptUser = () => {
         {
             type: 'list',
             name: 'entryOptions',
-            message: 'Please choose your option from the list: ',
+            message: 'What would you like to do? ',
             choices: entrySelect,
             validate: selection => {
                 if (selection) {
@@ -23,22 +46,31 @@ const promptUser = () => {
                     console.log('Please choose an option!');
                     return false;
                 }
-            }
+            },
+            default: 'View All Employees'
         },
         {
             type: 'input',
             name: 'employeeName',
             message: "What is the employee's name?",
-        },
-        {
-            type: 'number',
-            name: 'employeeAge',
-            message: "Your employee's age?"
+            validate: haveName => {
+                if (haveName) {
+                    return true;
+                } else {
+                    console.log("Please enter employee's name!");
+                    return false;
+                }
+            }
         }
+        // {
+        //     type: 'number',
+        //     name: 'employeeAge',
+        //     message: "Your employee's age?"
+        // }
     ]);
 }
 
 promptUser()
     .then(answer => {
-        console.log(answer);
+        console.table(answer);
     });
---
+++ utils/viewQuery.js
@@ -0,0 +1,3 @@
+const db = require('../db/connection');
+
+const viewAllEmployees = `SELECT * FROM employee;`;
